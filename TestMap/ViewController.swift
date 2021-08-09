fa4e98cb1a0c73566c12a7f7049f09ba26d1002a
youngmo <youngmo0806@naver.com>
2021-08-09 18:07:39 +0900

    2021/08/09 Second

--- TestMap/ViewController.swift
+++ TestMap/ViewController.swift
@@ -8,15 +8,55 @@
 import UIKit
 import Foundation
 import NMapsMap
+import CoreLocation
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //NMFMapView        -- 지도 제공 뷰
+        //NMFNaverMapView   -- 지도 제공 뷰 (확대/축소버튼 제공)
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        
+        //포그라운드일때 위치 추적 권한 요청
+        locationManager.requestWhenInUseAuthorization()
+        
+        //배터리에 맞게 권장되는 최적의 정확도
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        //위치 업데이트
+        locationManager.startUpdatingLocation()
+        
+        //위,경도 가져오기
+        let coor = locationManager.location?.coordinate
+        var latitude = coor?.latitude
+        var longitude = coor?.longitude
+        
+        print("위도:\(latitude!), 경도:\(longitude!)")
+        
+        
         
         let mapView = NMFMapView(frame: view.frame)
+//        var coder = NSCoder()
+        
+        
+        mapView.mapType = .basic
+        
+//        mapView.ma
+        
+        
         view.addSubview(mapView)
+        
+        
+        
+        
+        
+
         
     }
 
